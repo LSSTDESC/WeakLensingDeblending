6c939709cd01133dcb66037869b2efef5610639d
David Kirkby <dkirkby@uci.edu>
2013-05-06 17:43:16 +0200

    Completed migration of globals to optargs

--- galsimcat.py
+++ galsimcat.py
@@ -14,39 +14,6 @@ import argparse
 import logging
 import galsim
 
-# image pixel bounds
-xmin = ymin = 1792
-xmax = ymax = 2304
-##xmin = ymin = 0
-##xmax = ymax = 50
-
-pixelScale = 0.2 # arcsecs / pixel
-
-# Calculate RA,DEC bounds
-RAmin = xmin*pixelScale
-RAmax = xmax*pixelScale
-DECmin = ymin*pixelScale
-DECmax = ymax*pixelScale
-
-# Number of visits to simulate (source flux will be multiplied by 2*nvisits)
-nvisits = 230
-
-# average sky level to simulate in ADU units per pixel
-# see http://darkmatter.ps.uci.edu/wiki/LSST%20Exposure%20Calculations
-skyLevel = 780.778
-
-# Specify constant cosmic shear to apply across the field
-g1 = 0.0
-g2 = 0.0
-
-# Configure parameter variations
-dxyc = pixelScale/3.
-nxyc = 1
-dg12 = 0.03
-ng12 = 1
-dhlr = 0.02
-nhlr = 1
-
 def createSource(flux,rhalf,q,beta,g1,g2,dx,dy):
     source = galsim.Exponential(flux = flux, half_light_radius = rhalf)
     ##source = galsim.Gaussian(flux = flux, half_light_radius = rhalf)
@@ -56,18 +23,51 @@ def createSource(flux,rhalf,q,beta,g1,g2,dx,dy):
     return source
 
 def main(argv):
-    
+   
     # Parse command-line args
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", default='trim.dat', help="name of input catalog to read")
+    parser.add_argument("-i", "--input", default = 'trim.dat',
+        help = "name of input catalog to read")
+    parser.add_argument("--xmin", type = int, default = 1792,
+        help = "left edge of image (pixels)")
+    parser.add_argument("--xmax", type = int, default = 2304,
+        help = "right edge of image (pixels)")
+    parser.add_argument("--ymin", type = int, default = 1792,
+        help = "bottom edge of image (pixels)")
+    parser.add_argument("--ymax", type = int, default = 2304,
+        help = "top edge of image (pixels)")
+    parser.add_argument("--pixel-scale", type = float, default = 0.2,
+        help = "pixel scale (arscecs/pixel)")
+    parser.add_argument("--nvisits", type = int, default = 230,
+        help = "number of visits to simulate")
+    parser.add_argument("--sky-level", type = float, default = 780.778,
+        help = "average sky level to simulate (ADU/pixel)")
+    parser.add_argument("--g1", type = float, default = 0.,
+        help = "Constant shear component g1 to apply")
+    parser.add_argument("--g2", type = float, default = 0.,
+        help = "Constant shear component g2 to apply")
     args = parser.parse_args()
 
     # In non-script code, use getLogger(__name__) at module scope instead.
     logging.basicConfig(format="%(message)s", level=logging.INFO, stream=sys.stdout)
     logger = logging.getLogger("aliasing")
 
+    # Calculate RA,DEC bounds
+    RAmin = args.xmin*args.pixel_scale
+    RAmax = args.xmax*args.pixel_scale
+    DECmin = args.ymin*args.pixel_scale
+    DECmax = args.ymax*args.pixel_scale
+
+    # Configure parameter variations
+    dxyc = args.pixel_scale/3.
+    nxyc = 1
+    dg12 = 0.03
+    ng12 = 1
+    dhlr = 0.02
+    nhlr = 1
+
     # Define the image characteristics
-    pix = galsim.Pixel(pixelScale)
+    pix = galsim.Pixel(args.pixel_scale)
 
     # Open the input catalog to use
     cat = galsim.InputCatalog(args.input)
@@ -76,7 +76,7 @@ def main(argv):
     # Initialize the list of stamps we will create and an empty image that
     # we will add each stamp to
     stamps = [ ]
-    field = galsim.ImageD(xmax-xmin,ymax-ymin,init_value = 0)
+    field = galsim.ImageD(args.xmax-args.xmin,args.ymax-args.ymin,init_value = 0)
 
     # Define the psf (beta = 3 taken from GREAT10 simulations)
     psf = galsim.Moffat(beta = 3, fwhm = 0.7)
@@ -101,20 +101,20 @@ def main(argv):
         nkeep += 1
         
         # Scale flux to number of vists (extra factor of 2 because 1 visit = 2 exposures)
-        flux = 2*nvisits*flux
+        flux = 2*args.nvisits*flux
         
         # Calculate the offset of this stamp's central pixel (in pixels) in the full field
-        xc = int(round(RA/pixelScale))
-        yc = int(round(DEC/pixelScale))
+        xc = int(round(RA/args.pixel_scale))
+        yc = int(round(DEC/args.pixel_scale))
 
         # Calculate the subpixel shift of the object's center (in arcsecs) within the stamp
-        xshift = RA-pixelScale*xc
-        yshift = DEC-pixelScale*yc
+        xshift = RA-args.pixel_scale*xc
+        yshift = DEC-args.pixel_scale*yc
 
         # Calculate the stamp size to use. Always round up to an odd integer
         # so that flux is consistently centered (see Issue #380).
-        w = 2*int(math.ceil(width/pixelScale))+1
-        h = 2*int(math.ceil(height/pixelScale))+1
+        w = 2*int(math.ceil(width/args.pixel_scale))+1
+        h = 2*int(math.ceil(height/args.pixel_scale))+1
         ## make the stamp square
         #w = h = max(w,h)
         logger.info('rendering stamp %d (id %d) with size %d x %d' % (nkeep,k,w,h))
@@ -122,52 +122,52 @@ def main(argv):
         # Calculate the amount to shift this stamp so that it is correctly centered
         # in our image with bounds (xmin,ymin) - (xmax,ymax).
         ## Do these need an addition (1,1) offset?
-        dx = xc - (w-1)/2 - xmin
-        dy = yc - (h-1)/2 - ymin
+        dx = xc - (w-1)/2 - args.xmin
+        dy = yc - (h-1)/2 - args.ymin
 
         # Define the instrinsic galaxy profile
-        gal = createSource(flux,hlr,q,beta,g1,g2,xshift,yshift)
+        gal = createSource(flux,hlr,q,beta,args.g1,args.g2,xshift,yshift)
         
         # Create a copy that will not have any psf applied
-        nopsf = createSource(flux,hlr,q,beta,g1,g2,xshift,yshift)
+        nopsf = createSource(flux,hlr,q,beta,args.g1,args.g2,xshift,yshift)
 
         # Create a family of galaxies with small parameter variations
         variations = [ nopsf, gal ]
         for ixyc in range(1,nxyc+1):
             delta = ixyc*dxyc
             # Vary xc by +delta
-            copy = createSource(flux,hlr,q,beta,g1,g2,xshift+delta,yshift)
+            copy = createSource(flux,hlr,q,beta,args.g1,args.g2,xshift+delta,yshift)
             variations.append(copy)
             # Vary xc by -delta
-            copy = createSource(flux,hlr,q,beta,g1,g2,xshift-delta,yshift)
+            copy = createSource(flux,hlr,q,beta,args.g1,args.g2,xshift-delta,yshift)
             variations.append(copy)
             # Vary yc by +delta
-            copy = createSource(flux,hlr,q,beta,g1,g2,xshift,yshift+delta)
+            copy = createSource(flux,hlr,q,beta,args.g1,args.g2,xshift,yshift+delta)
             variations.append(copy)
             # Vary yc by -delta
-            copy = createSource(flux,hlr,q,beta,g1,g2,xshift,yshift-delta)
+            copy = createSource(flux,hlr,q,beta,args.g1,args.g2,xshift,yshift-delta)
             variations.append(copy)
         for ie12 in range(1,ng12+1):
             delta = ie12*dg12
             # Vary e1 by +delta
-            copy = createSource(flux,hlr,q,beta,g1+delta,g2,xshift,yshift)
+            copy = createSource(flux,hlr,q,beta,args.g1+delta,args.g2,xshift,yshift)
             variations.append(copy)
             # Vary e1 by -delta
-            copy = createSource(flux,hlr,q,beta,g1-delta,g2,xshift,yshift)
+            copy = createSource(flux,hlr,q,beta,args.g1-delta,args.g2,xshift,yshift)
             variations.append(copy)
             # Vary e2 by +delta
-            copy = createSource(flux,hlr,q,beta,g1,g2+delta,xshift,yshift)
+            copy = createSource(flux,hlr,q,beta,args.g1,args.g2+delta,xshift,yshift)
             variations.append(copy)
             # Vary e2 by -delta
-            copy = createSource(flux,hlr,q,beta,g1,g2-delta,xshift,yshift)
+            copy = createSource(flux,hlr,q,beta,args.g1,args.g2-delta,xshift,yshift)
             variations.append(copy)
         for ihlr in range(1,nhlr+1):
             delta = ihlr*dhlr
             # Vary hlr by +delta
-            copy = createSource(flux,hlr+delta,q,beta,g1,g2,xshift,yshift)
+            copy = createSource(flux,hlr+delta,q,beta,args.g1,args.g2,xshift,yshift)
             variations.append(copy)
             # Vary hlr by -delta
-            copy = createSource(flux,hlr-delta,q,beta,g1,g2,xshift,yshift)
+            copy = createSource(flux,hlr-delta,q,beta,args.g1,args.g2,xshift,yshift)
             variations.append(copy)
 
         # Loop over variations to render
@@ -177,7 +177,7 @@ def main(argv):
                 object = galsim.Convolve([src,pix], real_space = True)
             else:
                 object = galsim.Convolve([src,psf,pix])
-            object.draw(image = stamp, dx = pixelScale)
+            object.draw(image = stamp, dx = args.pixel_scale)
             stamp.shift(dx,dy)
             stamps.append(stamp)
             if src == gal:
@@ -191,9 +191,9 @@ def main(argv):
     galsim.fits.write(field,outname)
 
     # Write out the full field image with noise added
-    if skyLevel > 0:
+    if args.sky_level > 0:
         rng = galsim.BaseDeviate(123)
-        noise = galsim.PoissonNoise(rng,sky_level = 2*nvisits*skyLevel)
+        noise = galsim.PoissonNoise(rng,sky_level = 2*args.nvisits*args.sky_level)
         field.addNoise(noise)
         outname = 'fieldnoise.fits'
         logger.info('Saving full field to %r' % outname)
