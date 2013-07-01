641127b498c180e52fdf43dec1582cd563935066
David Kirkby <dkirkby@uci.edu>
2013-07-01 07:40:17 +0200

    Use argparse for args currently being captured on command line

--- lsst2wl.py
+++ lsst2wl.py
@@ -6,7 +6,7 @@
 ## Uses the pymsql package for talking to the remote Microsoft SQL server, which in turns requires
 ## that Cython and FreeTDS are installed.
 ##
-## Usage: lsst2wl.py <maxrows> <fname>
+## Usage example: lsst2wl.py --maxrows 100 --output galaxies.dat
 ##
 ## Created 07-Nov-2012 by David Kirkby <dkirkby@uci.edu>
 #######################################################################################################
@@ -16,25 +16,27 @@ import argparse
 
 def main():
 
-    if len(sys.argv) != 3:
-        print 'usage: %s <maxrows> <filename>' % sys.argv[0]
-        sys.exit(-1)
-        
-    try:
-        # The maximum number of rows to retrieve (total number in the catalog is ~17M)
-        maxrows = int(sys.argv[1])
-        if maxrows < 0:
-            maxrows = 100
-    except ValueError:
-        print 'Expected an integer for maxrows'
-        sys.exit(-1)
+    # Parser command-line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action = "store_true",
+        help = "provide more verbose output")
+    parser.add_argument("--maxrows", type = int, default = 100,
+        help = "maximum number of rows to fetch from the server")
+    parser.add_argument("-o", "--output", default = "gcat.dat",
+        help = "name of output catalog file to write")
+    args = parser.parse_args()
 
+    # Check for a valid maxrows (total rows in the DB is about 17M)
+    if args.maxrows <= 0:
+        print 'Invalid maxrows %r <= 0'
+        sys.exit(-2)
+
+    # Try to open the output file
     try:
         # The filename to write
-        fname = sys.argv[2]
-        f = open(fname,'w')
+        f = open(args.output,'w')
     except IOError,e:
-        print 'Cannot write to filename "%s"' % fname
+        print 'Cannot open output %r for writing' % args.output
         print str(e)
         sys.exit(-2)
 
@@ -61,9 +63,6 @@ def main():
     # Add filter-specific columns
     columns += addColumns(('%s_ab',),"ugrizy")
 
-    # Filename to write
-    fname = 'gcat.dat'
-
     # SQL filter to use
     filter = 'WHERE ra BETWEEN %(RAmin)s AND %(RAmax)s AND dec BETWEEN %(DECmin)s and %(DECmax)s' % window
 
@@ -76,7 +75,7 @@ def main():
             server='fatboy.npl.washington.edu', port=1433,
             user='LSST-2', password='L$$TUser',
             database='LSST')
-        conn.execute_query("SELECT TOP %d %s FROM galaxy %s" % (maxrows,columns,filter))
+        conn.execute_query("SELECT TOP %d %s FROM galaxy %s" % (args.maxrows,columns,filter))
         nrows = 0
         for row in conn:
             # Filter out any SQL NULLs
@@ -89,11 +88,12 @@ def main():
             # Dump this row to our output file
             print >>f, ' '.join([str(row[col]) for col in clist])
             nrows += 1
-        print 'Dumped',nrows,'rows to',fname
-        if nulls:
-            print 'Replaced NULLs with',nullSub,'for:'
-            for col in nulls:
-                print '%10d %s' % (nulls[col],col)
+        if args.verbose:
+            print 'Dumped',nrows,'rows to',args.output
+            if nulls:
+                print 'Replaced NULLs with',nullSub,'for:'
+                for col in nulls:
+                    print '%10d %s' % (nulls[col],col)
     except _mssql.MssqlDatabaseException,e:
         print 'Database Exception'
         raise
