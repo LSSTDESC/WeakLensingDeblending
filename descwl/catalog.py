d776dd89d11aea4fb0af2d4764b812b64ba9119a
David Kirkby <dkirkby@uci.edu>
2014-12-08 15:08:54 -0800

    Implement from_args as a classmethod

--- descwl/catalog.py
+++ descwl/catalog.py
@@ -9,7 +9,7 @@ class Reader(object):
     """Read source parameters from a catalog to simulate an image.
 
     The entire catalog is read when the object is constructed, independently of the
-    only_row and skip_row parameter values. The recommended method for iterating over
+    only_id and skip_id parameter values. The recommended method for iterating over
     catalog entries is::
 
         catalog = descwl.catalog.Reader(...)
@@ -22,24 +22,29 @@ class Reader(object):
         catalog_name(str): Name of catalog file, which must exist and be readable.
         ra_center(float): Right ascension of image center in degrees.
         dec_center(float): Declination of image center in degrees.
-        only_row(array): Only read rows in this array of integer row indices.
-        skip_row(array): Skip rows in this array of integer row indices.
+        only_id(array): Only read ids in this array of integer ids.
+        skip_id(array): Skip ids in this array of integer ids.
+
+    Raises:
+        RuntimeError: Missing required catalog_name arg.
     """
-    def __init__(self,catalog_name,ra_center,dec_center,only_row,skip_row):
-        self.only_row = only_row
-        self.skip_row = skip_row
+    def __init__(self,catalog_name,ra_center,dec_center,only_id,skip_id):
+        if not catalog_name:
+            raise RuntimeError('Missing required catalog_name arg.')
+        self.only_id = only_id
+        self.skip_id = skip_id
         self.table = ascii.read(catalog_name, format='basic')
 
     def __iter__(self):
         """Iterate over catalog entries.
 
-        If only_row has any entries, then only the specified rows will be considered. Otherwise,
-        all rows are considered. Any rows in skip_row will be silently ignored.
+        If only_id has any entries, then only the specified ids will be considered. Otherwise,
+        all ids are considered. Any ids in skip_id will be silently ignored.
         """
         for index,row in enumerate(self.table):
-            if self.only_row and index not in self.only_row:
+            if self.only_id and index not in self.only_id:
                 continue
-            if self.skip_row and index in self.skip_row:
+            if self.skip_id and index in self.skip_id:
                 continue
             yield row
 
@@ -61,13 +66,13 @@ class Reader(object):
             help = 'Right ascension of image center in degrees.')
         parser.add_argument('--dec-center', type = float, default = 0.0, metavar = 'DEC',
             help = 'Declination of image center in degrees.')
-        parser.add_argument('--only-row', type = int, action = 'append', metavar = 'N',
-            help = 'Use row N from the input catalog. May be repeated. All other lines will be ignored.')
-        parser.add_argument('--skip-row', type = int, action = 'append', metavar = 'N',
-            help = 'Skip row N from the input catalog. May be repeated.')
+        parser.add_argument('--only-id', type = int, action = 'append', metavar = 'ID',
+            help = 'Use row with ID from the input catalog. May be repeated. All other rows will be ignored.')
+        parser.add_argument('--skip-id', type = int, action = 'append', metavar = 'ID',
+            help = 'Skip row with ID from the input catalog. May be repeated.')
 
-    @staticmethod
-    def from_args(args):
+    @classmethod
+    def from_args(cls,args):
         """Create a new Reader object from a set of arguments.
 
         Args:
@@ -77,10 +82,9 @@ class Reader(object):
             Reader: A newly constructed Reader object.
         """
         # Look up the named constructor parameters.
-        pnames = (inspect.getargspec(Reader.__init__)).args[1:]
+        pnames = (inspect.getargspec(cls.__init__)).args[1:]
         # Get a dictionary of the arguments provided.
         args_dict = vars(args)
         # Filter the dictionary to only include constructor parameters.
         filtered_dict = { key:args_dict[key] for key in (set(pnames) & set(args_dict)) }
-        print filtered_dict
-        return Reader(**filtered_dict)
+        return cls(**filtered_dict)
--- simulate.py
+++ simulate.py
@@ -14,12 +14,17 @@ def main():
     descwl.catalog.Reader.add_args(catalog_group)
     args = parser.parse_args()
 
-    catalog = descwl.catalog.Reader.from_args(args)
-    entries = 0
-    for entry in catalog:
-        if entries < 10:
-            print entry
-        entries += 1
+    try:
+        catalog = descwl.catalog.Reader.from_args(args)
+        entries = 0
+        for entry in catalog:
+            if entries < 3:
+                print entry
+            entries += 1
+
+    except RuntimeError,e:
+        print str(e)
+        return -1
 
 if __name__ == '__main__':
     main()
