464d80028d63776f213af351a119e49a0e084fae
David Kirkby <dkirkby@uci.edu>
2014-12-08 11:42:52 -0800

    Create skeleton simulate program and descwl module

---
+++ descwl/__init__.py
@@ -0,0 +1,7 @@
+"""Weak lensing fast simulations for the Dark Energy Science Collaboration.
+
+This code was primarily developed to study the effects of overlapping sources on shear estimation,
+photometric redshift algorithms, and deblending algorithms.
+"""
+
+pass
---
+++ simulate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Fast image simulation using GalSim for weak lensing studies.
+"""
+
+import argparse
+
+import descwl
+
+def main():
+    # Parse command-line arguments.
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
