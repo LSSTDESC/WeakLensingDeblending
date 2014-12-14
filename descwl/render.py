0ee9a6dd612574d5c86ff3ed5da7ed49dfe14429
David Kirkby <dkirkby@uci.edu>
2014-12-14 14:44:12 -0800

    Render each model in its own postage stamp

--- descwl/render.py
+++ descwl/render.py
@@ -16,12 +16,14 @@ class Engine(object):
             set by the expected fluctuations in the sky background during a full exposure.
         truncate_size(float): Truncate sources to a square mask with this full size in arcseconds.
         no_margin(bool): Do not simulate the tails of objects just outside the field.
+        verbose_render(bool): Provide verbose output on rendering process.
     """
-    def __init__(self,survey,min_snr,truncate_size,no_margin):
+    def __init__(self,survey,min_snr,truncate_size,no_margin,verbose_render):
         self.survey = survey
         self.min_snr = min_snr
         self.truncate_size = truncate_size
         self.no_margin = no_margin
+        self.verbose_render = verbose_render
         # Calculate pixel flux threshold in electrons per pixel that determines how big a
         # bounding box we simulate for each source.
         sky_noise = math.sqrt(survey.get_sky_level())
@@ -38,7 +40,8 @@ class Engine(object):
         Returns:
             :class:`numpy.ndarray`: Array of shape (nstamp,width,height) pixel values that represent
                 nstamp postage-stamp images with the same dimensions (width,height) calculated
-                based on the rendering options provided.
+                based on the rendering options provided. Returns None if this galaxy has no
+                pixels above threshold that are visible in the simulated survey image.
         """
         # Calculate the offset of the source center from the bottom-left corner of the
         # simulated image in floating-point pixel units.
@@ -47,16 +50,47 @@ class Engine(object):
 
         # Calculate the bounding box extents to use in floating-point pixel units.
         # Use the maximum-sized stamp for now.
-        half_width_pixels = 0.5*self.truncate_size
-        half_height_pixels = 0.5*self.truncate_size
+        half_width_pixels = 0.5*self.truncate_size/self.survey.pixel_scale
+        half_height_pixels = 0.5*self.truncate_size/self.survey.pixel_scale
 
-        # Render the galaxy using the maximum sized stamp for now.
-        half_size = math.ceil(0.5*self.truncate_size/self.survey.pixel_scale)
-        bounding_box = galsim.BoundsI()
-        stamp = galsim.Image(2*half_size,2*half_size)
+        # Calculate the bounding box to use for simulating this galaxy.
+        x_min = int(math.floor(x_center_pixels - half_width_pixels))
+        y_min = int(math.floor(y_center_pixels - half_height_pixels))
+        # Use floor instead of ceil here since upper bounds are inclusive.
+        x_max = int(math.floor(x_center_pixels + half_width_pixels))
+        y_max = int(math.floor(y_center_pixels + half_height_pixels))
+        bounds = galsim.BoundsI(x_min,x_max,y_min,y_max)
 
-        model = galsim.Convolve([galaxy.model,self.survey.psf_model],gsparams=self.galsim_params)
-        model.drawImage(image = self.survey.image,add_to_image = True,use_true_center = True)
+        # Is there any overlap with the simulated image?
+        survey_overlap = bounds & self.survey.image.bounds
+        if survey_overlap.area() == 0:
+            return None
+
+        # Calculate the offset of the bounding box center from the image center in arcsecs.
+        dx_stamp_arcsec = 0.5*(x_min + x_max+1 - self.survey.image_width)*self.survey.pixel_scale
+        dy_stamp_arcsec = 0.5*(y_min + y_max+1 - self.survey.image_height)*self.survey.pixel_scale
+
+        # Shift the model to the bounding box center and convolve with the survey PSF.
+        model = galsim.Convolve([
+            galaxy.model.shift(dx=-dx_stamp_arcsec,dy=-dy_stamp_arcsec),
+            self.survey.psf_model
+            ],gsparams=self.galsim_params)
+
+        # Render the model in its own postage stamp.
+        stamp = galsim.Image(bounds = bounds,scale = self.survey.pixel_scale)
+        model.drawImage(image = stamp, use_true_center = True)        
+        self.survey.image[survey_overlap] += stamp[survey_overlap]
+
+        # Draw directly into the survey image, with no truncation.
+        #model.drawImage(image = self.survey.image,add_to_image = True,use_true_center = True)
+
+        if self.verbose_render:
+            print 'Rendering galaxy model for id = %d with z = %.3f' % (
+                galaxy.identifier,galaxy.redshift)
+            print 'bounds: [%d:%d,%d:%d] w,h = %d,%d' % (
+                x_min,x_max,y_min,y_max,x_max-x_min+1,y_max-y_min+1)
+            print ' shift: (%.6f,%.6f) arcsec relative to stamp center' % (
+                model.centroid().x,model.centroid().y)
 
     @staticmethod
     def add_args(parser):
@@ -76,6 +110,8 @@ class Engine(object):
             help = 'Truncate sources to a square mask with this full size in arcseconds.')
         parser.add_argument('--no-margin', action = 'store_true',
             help = 'Do not simulate the tails of objects just outside the field.')
+        parser.add_argument('--verbose-render', action = 'store_true',
+            help = 'Provide verbose output on rendering process.')
 
     @classmethod
     def from_args(cls,survey,args):
