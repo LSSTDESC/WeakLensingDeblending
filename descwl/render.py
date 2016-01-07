73aad28570b1da9288fba45bea325950c61711f9
ismael2395 <imendoza@stanford.edu>
2016-01-07 14:07:00 -0800

    Actually put things wld-change1 into render.py

--- descwl/render.py
+++ descwl/render.py
@@ -9,10 +9,6 @@ import numpy as np
 import galsim
 
 
-
-
-
-
 class SourceNotVisible(Exception):
     """Custom exception to indicate that a source has no visible pixels above threshold.
     """
@@ -236,22 +232,63 @@ class Engine(object):
             ('dg2',0.03), # x shear using |g| = (a-b)/(a+b) convention
             ]
 
-        # Prepare the datacube that we will return.
-        if no_partials:
-            ncube = 1
-        else:
-            # The nominal image doubles as the flux partial derivative.
-            ncube = 1+len(variations)
-        height,width = cropped_stamp.array.shape
-        datacube = np.empty((ncube,height,width))
-        datacube[0] = cropped_stamp.array
 
-        # Calculate partial derivative images, if requested.
-        if not no_partials:
-            for i,(pname,delta) in enumerate(variations):
-                variation_stamp = (galaxy.renderer.draw(**{pname: +delta}).copy() - 
-                    galaxy.renderer.draw(**{pname: -delta}))
-                datacube[i+1] = variation_stamp.array/(2*delta)
+            """
+            This function returns the correction position in the datacube of the specified 2nd 
+            partial derivative i,j. Note that we are filling in cubes index 6 to 19 
+            Follow the order in the variations dict. 
+            Differentiating partial flux with respect to flux gives 0. 
+            Differentiating any partial with respect to flux leaves it unchanged. 
+            Therefore ignore flux partials.
+            """
+            def index_partial(i,j, variations):
+                
+                #return sum of n, n-1, ..., k (non-excluding)
+                def sum_n_to_k(n,k):
+                    l = [x for x in range(n+1) if x>=k]
+                    return sum(l)
+
+                return sum_n_to_k(len(variations),len(variations)+2-i) + j - i + (1+len(variations)) 
+
+            # Prepare the datacube that we will return.
+            ncube = 1
+
+            if no_partials:
+                ncube = 1+len(variations)
+                if no_bias:
+                     #15 is number of second partials for 5 parameters (no flux).
+                    ncube = 1 + len(variations) + 15
+
+
+            height,width = cropped_stamp.array.shape
+            datacube = np.empty((ncube,height,width))
+            datacube[0] = cropped_stamp.array #flux partial is the same. 
+
+            #calculate partials, if requested.
+            if not no_partials:
+                  #The nominal image doubles as the flux partial derivative.
+                for i,(pname_i,delta_i) in enumerate(variations):
+
+                    variation_stamp = (galaxy.renderer.draw(**{pname: +delta_i}).copy() - 
+                                           galaxy.renderer.draw(**{pname: -delta_i}))
+                    datacube[i+1] = variation_stamp.array/(2*delta_i)
+
+                    #calculated second partials, if requested. 
+                    if not no_bias:        
+                        for j,(pname_j,delta_j) in range(len(variations))[i:],variations:
+                            galaxy_iup_jup = galaxy.renderer.draw(**{pname_i: +delta_i, 
+                                                                  pname_j: +delta_j})
+                            galaxy_iup_jdown = galaxy.renderer.draw(**{pname_i: +delta_i, 
+                                                                       pname_j: -delta_j})
+                            galaxy_idown_jup = galaxy.renderer.draw(**{pname_i: -delta_i, 
+                                                                       pname_j: +delta_j})
+                            galaxy_idown_jdown = galaxy.renderer.draw(**{pname_i: -delta_i, 
+                                                                         pname_j: -delta_j})
+
+                            variation_i_j = (galaxy_iup_jup + galaxy_idown_jdown - 
+                                             galaxy_idown_jup - galaxy_iup_jdown)
+                            datacube[index_partial(i+1,j+1, variations)] = (variation_i_j / 
+                                                                            (4*delta_i*delta_j))
 
         if self.verbose_render:
             print 'Rendered galaxy model for id = %d with z = %.3f' % (
@@ -283,8 +320,6 @@ class Engine(object):
             help = 'Do not simulate the tails of objects just outside the field.')
         parser.add_argument('--verbose-render', action = 'store_true',
             help = 'Provide verbose output on rendering process.')
-
-
 
         #add one for partials and bias. 
         parser.add_argument('--no_partials', action = 'store_true', 
--- wld-change1.py
+++ wld-change1.py
@@ -1,61 +1,52 @@
-#return sum of n, n-1, ..., k
-def sum_n_to_k(n,k):
-    l = [x for x in range(n+1) if x>=k]
-    return sum(l)
 
 """
-these function returns the correction position in the datacube of the specified partial derivative
-i,j. Note that we are filling in cubes number 7 to 20 and partials commute. Order established in 
-the variations dict.
+This function returns the correction position in the datacube of the specified 2nd 
+partial derivative i,j. Note that we are filling in cubes index 6 to 19 
+Follow the order in the variations dict. 
 Differentiating partial flux with respect to flux gives 0. 
 Differentiating any partial with respect to flux leaves it unchanged. 
-So we do not have to calculate any partials with respect to flux at all. 
+Therefore ignore flux partials.
 """
-def second_partials_indices_to_datacubes_indices(i,j):
-    return sum_n_to_k(5,7-i) + j - i + 7 
+def index_partial(i,j, variations):
+    
+    #return sum of n, n-1, ..., k (non-excluding)
+    def sum_n_to_k(n,k):
+        l = [x for x in range(n+1) if x>=k]
+        return sum(l)
 
+    return sum_n_to_k(len(variations),len(variations)+2-i) + j - i + (1+len(variations)) 
 
 # Prepare the datacube that we will return.
+ncube = 1
 
-if no_partials and no_bias:
-    ncube = 1
-
-elif not no_partials and no_bias:
-    # The nominal image doubles as the flux partial derivative.
+if no_partials:
     ncube = 1+len(variations)
+    if no_bias:
+         #15 is number of second partials for 5 parameters (no flux).
+        ncube = 1 + len(variations) + 15
 
-else:
-    ncube = 1 + len(variations) + 15 #number of second partials for 5 parameters (no flux).
 
 height,width = cropped_stamp.array.shape
 datacube = np.empty((ncube,height,width))
 datacube[0] = cropped_stamp.array #flux partial is the same. 
 
-#calculate partials and second partials, if requested. 
-if not no_partials and not no_bias:
+#calculate partials, if requested.
+if not no_partials:
+      #The nominal image doubles as the flux partial derivative.
     for i,(pname_i,delta_i) in enumerate(variations):
 
-        #partial
         variation_stamp = (galaxy.renderer.draw(**{pname: +delta_i}).copy() - 
-                               galaxy.renderer.draw(**{pname: -delta_j}))
+                               galaxy.renderer.draw(**{pname: -delta_i}))
         datacube[i+1] = variation_stamp.array/(2*delta_i)
 
-        if not no_bias:
-        
+        #calculated second partials, if requested. 
+        if not no_bias:        
             for j,(pname_j,delta_j) in range(len(variations))[i:],variations:
-
-                ##why the .copy()?? 
                 galaxy_iup_jup = galaxy.renderer.draw(**{pname_i: +delta_i, pname_j: +delta_j})
                 galaxy_iup_jdown = galaxy.renderer.draw(**{pname_i: +delta_i, pname_j: -delta_j})
                 galaxy_idown_jup = galaxy.renderer.draw(**{pname_i: -delta_i, pname_j: +delta_j})
                 galaxy_idown_jdown = galaxy.renderer.draw(**{pname_i: -delta_i, pname_j: -delta_j})
 
-                partial_i_j = galaxy_iup_jup + galaxy_idown_jdown - galaxy_idown_jup - galaxy_iup_jdown
-                datacube[f(i,j)] = partial_i_j
-
-# Calculate partial derivative images only, if requested.
-if not no_partials:
-    for i,(pname,delta) in enumerate(variations):
-        variation_stamp = (galaxy.renderer.draw(**{pname: +delta}).copy() - 
-            galaxy.renderer.draw(**{pname: -delta}))
-        datacube[i+1] = variation_stamp.array/(2*delta)
+                variation_i_j = (galaxy_iup_jup + galaxy_idown_jdown - 
+                                 galaxy_idown_jup - galaxy_iup_jdown)
+                datacube[index_partial(i+1,j+1, variations)] = variation_i_j / (4*delta_i*delta_j)
